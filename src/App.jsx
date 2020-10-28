23a93074c01973fafe5c0a256a45f9f5f5ffe227
pmayur <thisispatil@gmail.com>
2020-10-28 14:59:28 +0530

    Removes default generated files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>
+              react app
+            </h1>
+        </div>
+    );
+}
+
+export default App;
